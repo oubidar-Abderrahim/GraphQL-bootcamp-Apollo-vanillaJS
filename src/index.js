ed8f81bf4f46bd3786f14cc67572c6cedfb5d8bc
oubidar-Abderrahim <oubidar.abderrahim@gmail.com>
2019-02-13 15:29:00 +0100

    add some comments

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "./styles.css";
 import ApolloClient from "apollo-boost";
 import gql from "graphql-tag";
 
+// using graphQl tag to englobe our query
 const getPosts = gql`
   query getPosts {
     posts(order_by: { timestamp: desc }) {
@@ -17,14 +18,18 @@ const getPosts = gql`
   }
 `;
 
+// our Apollo client taking in charge the Post request
 const client = new ApolloClient({
+  // keep in mind that the link is a free tier server so it might be down sometimes
   uri: "https://graphql-bootcamp-sample-blog.herokuapp.com/v1alpha1/graphql"
 });
 
+// here we only show the returned object in the console
 client
   .query({ query: getPosts })
   .then(res => console.log(res))
   .catch(e => console.log(e));
+
 
 document.getElementById("app").innerHTML = `
 <h1>Hello Vanilla!</h1>
